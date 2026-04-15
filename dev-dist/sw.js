4d0db2bf05fd2376907442b7891e5fa4f0f29a1b
Sanjib Mondal <sanjib@technoexponent.com>
2026-04-15 18:11:43 +0530

    rename url key

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.com5ccmolvo"
+    "revision": "0.1a02dtlgs1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
